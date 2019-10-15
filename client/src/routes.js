8ab120d06e43686058f8da291c95bde94c57da49
Joe <josephc.lambert@gmail.com>
2019-10-15 16:24:47 -0700

    adds Friends component to routes as a private route

--- client/src/routes.js
+++ client/src/routes.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import PrivateRoute from './components/auth/PrivateRoute';
 
 import LandingPage from './components/LandingPage';
 import Login from './components/auth/Login';
+import Friends from './components/private/Friends';
 
 const Routes = () => (
   <Switch>
     <Route path='/login' component={Login} />
+    <PrivateRoute path='/friends' component={Friends} />
     <Route component={LandingPage} />
   </Switch>
 );
