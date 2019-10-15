2403888a603521479594c16ca6a0aa0216e0eed8
Joe <josephc.lambert@gmail.com>
2019-10-15 16:22:45 -0700

    creates private components directory

---
+++ client/src/components/private/Friends.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Friends = () => {
+  return (
+    <div>
+      <h2>This is the super secret friends page, only for ~~authenticated~~ friends</h2>
+    </div>
+  );
+};
+
+export default Friends;
