b9c5a5e5861f1c2c3785d29039a15d7a68318ad3
Joe <josephc.lambert@gmail.com>
2019-10-17 10:02:59 -0700

    Edit link routes user to edit form for the selected resource. Route returns null if the resource id doesn't match the route's param.id

--- client/src/components/private/FriendCardContainer.js
+++ client/src/components/private/FriendCardContainer.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Link, Route, useParams, Redirect } from 'react-router-dom';
+import { Link, Route, useParams, useHistory } from 'react-router-dom';
 import useForm from 'react-hook-form';
 
 import { updateFriend } from '../../api';
@@ -13,104 +13,107 @@ import {
 
 const EditFriend = props => {
   const { id } = useParams();
+  const { push } = useHistory();
   const dispatch = useDispatch();
-  const { register, handleSubmit, errors, watch } = useForm();
+  const { register, handleSubmit, errors } = useForm();
 
-  // useEffect(() => {
-  //   console.log('edit friend useEffect');
-  //   dispatch({ type: FRIENDS_EDITING_START, payload: id });
-  // }, [dispatch, id]);
-
-  if (Number(id) !== props.id) return null;
+  useEffect(() => {
+    !props.editing && dispatch({ type: FRIENDS_EDITING_START, payload: id });
+  }, [dispatch, id, props.editing]);
 
   const onSubmit = formValues => {
-    console.log('form values ', formValues);
-    updateFriend(formValues, props.id, {
-      start: payload => dispatch({ type: FRIENDS_UPDATE_START, payload }),
-      success: payload => dispatch({ type: FRIENDS_UPDATE_SUCCESS, payload }),
-      error: payload => dispatch({ type: FRIENDS_UPDATE_FAILURE, payload }),
-    });
+    updateFriend(
+      formValues,
+      props.id,
+      {
+        start: payload => dispatch({ type: FRIENDS_UPDATE_START, payload }),
+        success: payload => dispatch({ type: FRIENDS_UPDATE_SUCCESS, payload }),
+        error: payload => dispatch({ type: FRIENDS_UPDATE_FAILURE, payload }),
+      },
+      () => push('/friends')
+    );
   };
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div>
-        <input
-          type='text'
-          placeholder='Name'
-          name='name'
-          ref={register({ required: true })}
-          defaultValue={props.name}
-        />
-        <p>{errors.name && 'A name is required'}</p>
-      </div>
-      <div>
-        <input type='number' placeholder='Age' name='age' ref={register({ required: true })} defaultValue={props.age} />
-        <p>{errors.age && 'An age is required'}</p>
-      </div>
-      <div>
-        <input
-          type='email'
-          placeholder='Email'
-          name='email'
-          ref={register({ required: true, pattern: /^\S+@\S+$/i })}
-          defaultValue={props.email}
-        />
-        <p>{errors.name && 'A valid email is required'}</p>
-      </div>
-      <div>
-        <input type='submit' />
-      </div>
-    </form>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <input
+            type='text'
+            placeholder='Name'
+            name='name'
+            ref={register({ required: true })}
+            defaultValue={props.name}
+          />
+          <p>{errors.name && 'A name is required'}</p>
+        </div>
+        <div>
+          <input
+            type='number'
+            placeholder='Age'
+            name='age'
+            ref={register({ required: true })}
+            defaultValue={props.age}
+          />
+          <p>{errors.age && 'An age is required'}</p>
+        </div>
+        <div>
+          <input
+            type='email'
+            placeholder='Email'
+            name='email'
+            ref={register({ required: true, pattern: /^\S+@\S+$/i })}
+            defaultValue={props.email}
+          />
+          <p>{errors.name && 'A valid email is required'}</p>
+        </div>
+        <div>
+          <input type='submit' />
+        </div>
+      </form>
+      <Link to={`/friends`}>Cancel Editing</Link>
+    </>
   );
 };
 
 const FriendCard = props => {
-  // const handleClick = id => {
-  //   console.log('dispatch action to flip editing toggle and set editing id');
-  //   dispatch({ type: FRIENDS_EDITING_START, payload: id });
-  // };
-
-  // if (props.friend.editing && props.friend.id === props.id)
-  //   return <Route path='/friends/edit/:id' render={() => <EditFriend {...props} />} />;
-  // console.log(`Are we editing? --> ${props.friend.editing && Number(props.friend.id) === props.id ? 'yes' : 'no'}`);
   const editing = props.friend.editing && Number(props.friend.id) === props.id;
-
-  let Card = (
-    <>
-      <h3>{props.name}</h3>
-      <ul>
-        <li>{props.age}</li>
-        <li>{props.email}</li>
-      </ul>
-      <Link to={`/friends/edit/${props.id}`}>Edit Link</Link>
-    </>
-  );
-  if (editing) Card = null;
-
   return (
     <figure>
-      <div>{Card}</div>
+      {!editing && (
+        <>
+          <h3>{props.name}</h3>
+          <ul>
+            <li>{props.age}</li>
+            <li>{props.email}</li>
+          </ul>
+          <Link to={`/friends/edit/${props.id}`}>Edit</Link>
+        </>
+      )}
       <div>
-        <Route path='/friends/edit/:id' render={() => <EditFriend {...props} />} />
+        <Route
+          path='/friends/edit/:id'
+          render={({ match: { params } }) => {
+            return Number(params.id) === props.id && <EditFriend {...props} />;
+          }}
+        />
       </div>
     </figure>
   );
 };
 
 const FriendCardContainer = props => {
-  // This component will display the Friend Card, rendered inside the FriendsContainer
-  // const friend = useSelector(state => state.friends.friends.find(f => f.id === props.id));
   const friend = useSelector(state => state.friends.updateFriend);
-  // The Friend Card can be in one of two states:
-  // 1. Editing
-  // 2. Not Editing
+  // const editing = friend.editing && Number(friend.id) === props.id;
+  // const handleClick = id => {
+  //   dispatch({ type: FRIENDS_EDITING_START, payload: id });
+  // };
 
-  // We'll need to connect to the redux store to gain access to the above states
-  // so that we can render the correct component depending on the state
-
-  // pass the editing prop to a child
-  return <FriendCard {...props} friend={friend} />;
+  return (
+    <>
+      <FriendCard {...props} friend={friend} />
+    </>
+  );
 };
 
 export default FriendCardContainer;
