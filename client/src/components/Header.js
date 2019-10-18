423699bdd7a301a14934ffb49835df29707ea407
Joe <josephc.lambert@gmail.com>
2019-10-17 19:59:24 -0700

    renders logout link if user is logged in, or login link if user is not logged in.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,21 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 const Header = () => {
+  const user = useSelector(state => state.user);
+
+  const LogLink = user.isLoggedIn ? (
+    <Link to="/">Logout</Link>
+  ) : (
+    <Link to="/login">Login</Link>
+  );
+
   return (
     <header>
       <h1>Auth Friends</h1>
       <nav>
-        <Link to='login'>Login</Link>
-        <Link to='friends'>Friends</Link>
+        {LogLink}
+        <Link to="/friends">Friends</Link>
       </nav>
     </header>
   );
